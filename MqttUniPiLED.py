37ae6bc6e4ee5ba9c0fd7c2466e5ac370b4eb8ff
Robert Theiler <robert.theiler@etronicon.ch>
2019-10-19 00:25:04 +0200

    final

--- MqttUniPiLED.py
+++ MqttUniPiLED.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+    
 import paho.mqtt.client as mqtt
 from jsonrpclib import Server
 from thread import start_new_thread
 import time
 
+LastDimValue = 50
+print("Start Mqtt Unipi LED")
 unipi=Server("http://127.0.0.1:8100/rpc")
-kitchenDimVal = 0
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -15,12 +16,25 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
     if "reduitlamp" in msg.topic:
-        onoff=int(msg.payload)
+        onoff = 0
+        if "ON" in msg.payload:
+            onoff = 1
+        elif "OFF" in msg.payload:
+            onoff = 0
+        else:
+            onoff=int(msg.payload)
         switchReduitLamp(onoff)
     if "kitchenlamp" in msg.topic:
-        dimval=int(msg.payload)
+        dimval = 0
+        global LastDimValue
+        if "ON" in msg.payload:
+            dimval = LastDimValue
+        elif "OFF" in msg.payload:
+            dimval = 0
+        else:
+            dimval=int(msg.payload)
+            LastDimValue = dimval
         switchKitchenSpots(dimval)
-        print("set dim val of ktichen to "+str(kitchenDimVal))
 
 
 def switchKitchenSpots(dimValue):
@@ -48,5 +62,13 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("localhost", 1883, 60)
+
+switchKitchenSpots(0)
+time.sleep(0.5)
+switchKitchenSpots(50)
+time.sleep(0.5)
+switchKitchenSpots(100)
+time.sleep(0.5)
+switchKitchenSpots(0)
 
 client.loop_forever()
